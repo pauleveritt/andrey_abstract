735258d8cd234162de49aed437c9e62447be2821
Paul Everitt <pauleveritt@me.com>
2021-01-27 14:46:02 -0500

    Move the field sniffing to a standalone function with a test. Change the construction to use it.

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import inspect
 from dataclasses import dataclass
-from typing import Type, TypeVar, Dict
+from inspect import signature
+from typing import Type, TypeVar, Dict, NamedTuple, Any, Optional
+
+T = TypeVar('T')
+
+
+class FieldInfo(NamedTuple):
+    """ Extract needed info from dataclass fields, functions, etc. """
+
+    field_name: str
+    field_type: Type[T]
+    default_value: Optional[Any]
+
+
+FieldInfos = list[FieldInfo]
+
+
+def get_field_infos(target: type) -> FieldInfos:
+    results = []
+    empty = getattr(inspect, '_empty')
+    for value in signature(target).parameters.values():
+        name = value.name
+        name_type = value.annotation
+        default = value.default
+        results.append(FieldInfo(
+            field_name=name,
+            field_type=name_type,
+            default_value=default if default is not empty else None
+        ))
+    return results
 
 
 class View:
@@ -24,7 +53,7 @@ class Logo:
         return f'<img src="{self.config.logo_path}"/>'
 
 
-def test_1():
+def test_logo():
     logo = Logo(Config())
     assert logo.config.logo_path == 'default.png'
     assert logo.render() == '<img src="default.png"/>'
@@ -38,7 +67,9 @@ class Header:
         return f'<h1>{self.logo.render()}</h1>'
 
 
-T = TypeVar('T')
+def test_field_infos():
+    field_infos = get_field_infos(Header)
+    assert Logo == field_infos[0].field_type
 
 
 class Registry:
@@ -49,20 +80,27 @@ class Registry:
         }
 
     def get_component(self, interface: Type[T]) -> T:
-        cls = self.dict.get(interface, interface)
+        # Use the interface to get the implementation class we want to make
+        target = self.dict.get(interface, interface)
+
+        # Use inspect to find what values that class wants
         kwargs = {}
-        for value in inspect.signature(cls).parameters.values():
-            name = value.name
-            name_type = value.annotation
-            default = value.default
-            if default is not getattr(inspect, '_empty'):
-                kwargs[name] = default
+        for field_info in get_field_infos(target):
+            if field_info.default_value is not None:
+                # This field has a default value, let's presume it
+                # isn't something to look up. The actual rule here
+                # is more complicated.
+                field_value = field_info.default_value
             else:
-                kwargs[name] = self.get_component(name_type)
-        return cls(**kwargs)
+                field_value = self.get_component(field_info.field_type)
+
+            kwargs[field_info.field_name] = field_value
+
+        # Construct and return the class
+        return target(**kwargs)
 
 
-def test_2():
+def test_header():
     registry = Registry()
     header = registry.get_component(Header)
     assert header.logo.config.logo_path == 'default.png'
